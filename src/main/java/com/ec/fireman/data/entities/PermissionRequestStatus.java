211f095cbc248ab1ba9706eb2477f2b33d88a653
Edilberto <eblez@devsu.com>
2019-06-29 22:34:30 -0500

    In the commit some things were changed: - Was removed Permission entity - Was removed PermissionStatus enum - Was added PermissionRequestStatusDao - Was populated PermissionRequestStatus with some new status - Was fixed some things into client, inspector and economic modules

--- src/main/java/com/ec/fireman/data/entities/PermissionRequestStatus.java
+++ src/main/java/com/ec/fireman/data/entities/PermissionRequestStatus.java
@@ -1,5 +1,27 @@
 package com.ec.fireman.data.entities;
 
 public enum PermissionRequestStatus {
-  SUBMITTED, IN_PROGRESS, CLOSED, INSPECTED, PERMISSION_ISSUED
+  TO_REQUEST("Por Solicitar"), REQUESTED("Solicitado"),
+
+  IN_PROGRESS("En proceso de inspección"),
+
+  REJECTED("Rechazado"), TO_PAIED("Pendiente de pago"),
+
+  PERMISSION_ISSUED("Permiso emitido"),
+
+  TO_EXPIRE("Por caducar"), EXPIRED("Expirado");
+
+  private String value;
+
+  PermissionRequestStatus(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
 }

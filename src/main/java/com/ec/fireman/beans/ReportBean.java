b6aeecf5f802ae7e092a0f8836f9d8b5bdaba0bd
Osmany Castro Espinosa <osmanucoce@gmail.com>
2019-07-06 01:00:36 -0500

    Add partial report template

--- src/main/java/com/ec/fireman/beans/ReportBean.java
+++ src/main/java/com/ec/fireman/beans/ReportBean.java
@@ -24,12 +24,14 @@ import com.ec.fireman.util.MessageUtil;
 
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
+import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Data
@@ -50,51 +52,64 @@ public class ReportBean implements Serializable {
   @PostConstruct
   public void init() {
   }
-  
-  public void buildReport(ActionEvent event) {
-    //buscar info de la inspeccion
+
+  public void buildReport() {
+    // buscar info de la inspeccion
     InspectionHeader inspection = inspectionHeaderDao.findById(1);
-    //buscar data de extintores por inspeccion
-    List<InspectionFireExtinguisher> extinguishers =  inspectionFireExtinguisherDao.findAll();
+    // buscar data de extintores por inspeccion
+    List<InspectionFireExtinguisher> extinguishers = inspectionFireExtinguisherDao.findAll();
 
-    if (inspection != null) {
-      String nombreArchivo = "INSPECCION";
+    // if (inspection != null) {
+    String nombreArchivo = "INSPECCION";
 
-      Map<String, Object> params = new HashMap<String, Object>();
-      params.put("concrete", inspection.isConcrete());
-      params.put("metallicStructure", inspection.isMetallicStructure());
-      params.put("mixed", inspection.isMixed());
-      params.put("block", inspection.isBlock());
-      params.put("brick", inspection.isBrick());
-      params.put("adobe", inspection.isAdobe());
-      params.put("installationsGood", inspection.isInstallationsGood());
-      params.put("installationsBad", inspection.isInstallationsBad());
-      params.put("installationsInternal", inspection.isInstallationsInternal());
-      params.put("installationsExternal", inspection.isInstallationsExternal());
-      params.put("ventilationNatural", inspection.isVentilationNatural());
-      params.put("ventilationMechanic", inspection.isVentilationMechanic());
-      params.put("ventilationAdequate", inspection.isVentilationAdequate());
-      params.put("ventilationScarce", inspection.isVentilationScarce());
-      params.put("knowledgeExtinction", inspection.isKnowledgeExtinction());
-      params.put("alarms", inspection.isAlarms());
-      params.put("fireDetectors", inspection.isFireDetectors());
-      params.put("smokeDetectors", inspection.isSmokeDetectors());
-      params.put("emergencyLights", inspection.isEmergencyLights());
-      params.put("riskFire", inspection.getRiskFire());
-      params.put("recommendations", inspection.getRecommendations());
-      params.put("observations", inspection.getObservations());
+    Map<String, Object> params = new HashMap<String, Object>();
+    params.put("concrete", true);
+    params.put("metallicStructure", true);
+    params.put("mixed", false);
+    params.put("block", false);
+    params.put("brick", true);
+    params.put("adobe", false);
+    params.put("installationsGood", true);
+    params.put("installationsBad", false);
+    params.put("installationsInternal", false);
+    params.put("installationsExternal", true);
 
-      JRBeanCollectionDataSource data = extinguishers != null && !extinguishers.isEmpty() ? new JRBeanCollectionDataSource(extinguishers) : null;
+    // params.put("concrete", inspection.isConcrete());
+    // params.put("metallicStructure", inspection.isMetallicStructure());
+    // params.put("mixed", inspection.isMixed());
+    // params.put("block", inspection.isBlock());
+    // params.put("brick", inspection.isBrick());
+    // params.put("adobe", inspection.isAdobe());
+    // params.put("installationsGood", inspection.isInstallationsGood());
+    // params.put("installationsBad", inspection.isInstallationsBad());
+    // params.put("installationsInternal", inspection.isInstallationsInternal());
+    // params.put("installationsExternal", inspection.isInstallationsExternal());
+    // params.put("ventilationNatural", inspection.isVentilationNatural());
+    // params.put("ventilationMechanic", inspection.isVentilationMechanic());
+    // params.put("ventilationAdequate", inspection.isVentilationAdequate());
+    // params.put("ventilationScarce", inspection.isVentilationScarce());
+    // params.put("knowledgeExtinction", inspection.isKnowledgeExtinction());
+    // params.put("alarms", inspection.isAlarms());
+    // params.put("fireDetectors", inspection.isFireDetectors());
+    // params.put("smokeDetectors", inspection.isSmokeDetectors());
+    // params.put("emergencyLights", inspection.isEmergencyLights());
+    // params.put("riskFire", inspection.getRiskFire());
+    // params.put("recommendations", inspection.getRecommendations());
+    // params.put("observations", inspection.getObservations());
 
-      try {
-        this.generatePDF(nombreArchivo, "inspection.jrxml", data, params);
-      } catch (Exception e) {
-        log.error(e.getMessage());
-        MessageUtil.errorFacesMessage("Reporte", "Error al generar el reporte.");
-      }
-    } else {
-      MessageUtil.warningFacesMessage("Reporte", "No existe información");
+    JRBeanCollectionDataSource data = extinguishers != null && !extinguishers.isEmpty()
+        ? new JRBeanCollectionDataSource(extinguishers)
+        : null;
+
+    try {
+      this.generatePDF(nombreArchivo, "inspection.jrxml", data, params);
+    } catch (Exception e) {
+      log.error(e.getMessage());
+      MessageUtil.errorFacesMessage("Reporte", "Error al generar el reporte.");
     }
+    // } else {
+    // MessageUtil.warningFacesMessage("Reporte", "No existe información");
+    // }
   }
 
   public void generatePDF(String fileName, String templateName, JRBeanCollectionDataSource data,
@@ -102,10 +117,11 @@ public class ReportBean implements Serializable {
     try {
       FacesContext context = FacesContext.getCurrentInstance();
       String path = context.getExternalContext().getRealPath(File.separator) + File.separator + "resources"
-          + File.separator + "reportes" + File.separator + templateName;
+          + File.separator + "reports" + File.separator + templateName;
       HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
       JasperReport jasperReport = JasperCompileManager.compileReport(path);
-      JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, data);
+      JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params,
+          data != null ? data : new JREmptyDataSource());
       response.addHeader("Content-disposition", "attachment; filename=" + fileName + ".pdf");
 
       ServletOutputStream servletOutputStream = response.getOutputStream();
@@ -114,6 +130,7 @@ public class ReportBean implements Serializable {
       servletOutputStream.flush();
       servletOutputStream.close();
       context.responseComplete();
+
     } catch (Exception e) {
       log.error(e.getMessage());
     }
